659ec7366defd07fc5f467789b51aed4a37cea41
mikr <michael.krimgen@ximedes.com>
2017-12-01 09:27:34 +0100

    Puzzle 1.1 from 2017

---
+++ src/year2016/year2017/puzzle_1_1.kt
@@ -0,0 +1,25 @@
+package year2016.year2017
+
+import java.io.File
+import java.util.*
+
+fun main(args : Array<String>) {
+
+
+    val s = Scanner(File("resources/2017/input_1_1.txt")!!).useDelimiter("")
+
+    val first = Integer.parseInt(s.next())
+    var prev = first
+    var sum = 0
+
+    while(s.hasNext()) {
+        var cur = Integer.parseInt(s.next())
+        sum += (if(cur == prev) prev else 0)
+        prev = cur
+    }
+
+    sum += (if(prev == first) prev else 0)
+
+    println(sum)
+
+}
